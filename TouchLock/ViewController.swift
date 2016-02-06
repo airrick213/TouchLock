23ef44e1a300890541098280c32ce303a959b94a
Eric Kim <erickim213@gmail.com>
2016-02-06 13:21:59 -0500

    Initial Commit

---
+++ TouchLock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TouchLock
+//
+//  Created by Eric Kim on 2/6/16.
+//  Copyright © 2016 erickim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
